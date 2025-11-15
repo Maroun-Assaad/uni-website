dfc8bd65493d0e8c2820000a5bfc024ace076e42
Maroun <assaadmaroun2004@gmail.com>
2025-11-15 18:09:38 +0200

    few fixes in the images and ui in the home page

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,29 @@
+import hero1 from "./images/heroslider1.jpg";
+import hero2 from "./images/heroslider2.jpg";
+import hero3 from "./images/heroslider3.jpg";
+import hero4 from "./images/heroslider4.jpg";
 import sbj1 from "./images/subject-icon-1.png";
 import sbj2 from "./images/subject-icon-2.png";
 import sbj3 from "./images/subject-icon-3.png";
 import sbj4 from "./images/subject-icon-4.png";
 import sbj5 from "./images/subject-icon-5.png";
 import sbj6 from "./images/subject-icon-6.png";
+import c1 from "./images/course-2-1.jpg";
+import c2 from "./images/course-2-2.jpg";
+import c3 from "./images/course-2-3.jpg";
+import c4 from "./images/course-2-4.jpg";
+import c5 from "./images/course-2-5.jpg";
+import c6 from "./images/course-2-6.jpg";
+import c7 from "./images/course-2-7.jpg";
+import c8 from "./images/course-2-8.jpg";
+import c9 from "./images/course-2-9.jpg";
+
+const hereos = {
+  hero1,
+  hero2,
+  hero3,
+  hero4,
+};
 
 const subjects = {
   sbj1,
@@ -14,4 +34,16 @@ const subjects = {
   sbj6,
 };
 
-export { subjects };
+const courses = {
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+};
+
+export { subjects, hereos, courses };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { subjects } from "../assets/assets";
+import {
+  subjects as subAssets,
+  hereos,
+  courses as courseAssets,
+} from "../assets/assets";
+
 import "./Home.css";
 
 interface Subject {
@@ -28,37 +33,37 @@ const Home: React.FC = () => {
       id: 1,
       title: "Graphic Design",
       modules: 11,
-      image: "subjects.sbj1",
+      image: subAssets.sbj1,
     },
     {
       id: 2,
       title: "Mathematics",
       modules: 12,
-      image: "subjects.sbj2",
+      image: subAssets.sbj2,
     },
     {
       id: 3,
       title: "Teaching",
       modules: 12,
-      image: "subjects.sbj3",
+      image: subAssets.sbj3,
     },
     {
       id: 4,
       title: "Development",
       modules: 14,
-      image: "subjects.sbj4",
+      image: subAssets.sbj4,
     },
     {
       id: 5,
       title: "Science",
       modules: 13,
-      image: "subjects.sbj5",
+      image: subAssets.sbj5,
     },
     {
       id: 6,
       title: "Engineering",
       modules: 10,
-      image: "subjects.sbj6",
+      image: subAssets.sbj6,
     },
   ];
 
@@ -68,51 +73,46 @@ const Home: React.FC = () => {
       title: "Web Development",
       description:
         "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.",
-      image: "images/course-1-1.png",
+      image: courseAssets.c1,
     },
     {
       id: 2,
       title: "Engineering",
       description:
         "Engineering is the use of scientific principles to design and build machines, structures, and other items, including bridges, tunnels, roads, vehicles, and buildings.",
-      image: "images/course-1-2.png",
+      image: courseAssets.c2,
     },
     {
       id: 3,
       title: "Mathematics",
       description:
         "Mathematics is the study of representing and reasoning about abstract objects (such as numbers, points, spaces, sets, structures, and games). Mathematics is used throughout the world as an essential tool in many fields, including natural science, engineering, medicine, and the social sciences.",
-      image: "images/course-1-3.png",
+      image: courseAssets.c3,
     },
     {
       id: 4,
       title: "Science",
       description:
         "Science is a systematic endeavor that builds and organizes knowledge in the form of testable explanations and predictions about the universe.",
-      image: "images/course-1-4.png",
+      image: courseAssets.c5,
     },
     {
       id: 5,
       title: "Teaching",
       description:
         "Teaching is the practice implemented by a teacher aimed at transmitting skills (knowledge, know-how, and interpersonal skills) to a learner, a student, or any other audience in the context of an educational institution.",
-      image: "images/course-1-5.png",
+      image: courseAssets.c7,
     },
     {
       id: 6,
-      title: "Designing",
+      title: "Dancing",
       description:
-        "A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process or the result of that plan or specification in the form of a prototype, product, or process. The verb to design expresses the process of developing a design.",
-      image: "images/course-1-6.png",
+        "Dance is a performing art form consisting of purposefully selected sequences of human movement. This movement has aesthetic and symbolic value, and is acknowledged as dance by performers and observers within a particular culture.",
+      image: courseAssets.c9,
     },
   ];
 
-  const heroSlides = [
-    "https://images.unsplash.com/photo-1573497491208-6b1acb260507",
-    "https://images.unsplash.com/photo-1531297484001-80022131f5a1",
-    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f",
-    "https://images.unsplash.com/photo-1519389950473-47ba0277781c",
-  ];
+  const heroSlides = [hereos.hero1, hereos.hero2, hereos.hero3, hereos.hero4];
 
   useEffect(() => {
     const interval = setInterval(() => {
